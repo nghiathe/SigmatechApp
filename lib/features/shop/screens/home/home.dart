5428cc1bcff3f342d9ce2c66c758a8503d2a9791
HungTuLenh <ngduyhung811@gmail.com>
2024-12-24 21:54:54 +0700

    update: appbar

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:sigmatech/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:sigmatech/utils/constants/colors.dart';
 
 
+import '../../../../common/widgets/appbar/appbar.dart';
 import '../../../../common/widgets/custom_shapes/containers/circular_container.dart';
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 import '../../../../common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+import '../../../../common/widgets/products.cart/cart_menu_icon.dart';
+import '../../../../utils/constants/text_strings.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -16,7 +22,9 @@ class HomeScreen extends StatelessWidget {
           children: [
             TPrimaryHeaderContainer(
               child: Column(
-                children: [],
+                children: [
+                  THomeAppBar(),
+                ],
               ),)
           ]
 
@@ -26,6 +34,7 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
 
 
 
